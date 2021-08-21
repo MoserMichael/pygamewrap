ff9e76a52b59b394151cbe3e6352d86cd8d761a5
Michael Moser <moser.michael@gmail.com>
2021-08-21 21:14:51 +0300

    prepare pip build

--- build-pip.sh
+++ build-pip.sh
@@ -4,3 +4,24 @@ set -ex
 
 # remove files not under git
 git clean -f -d
+
+mkdir -p staging_dir/src/pywrap 
+mkdir -p staging_dir/tests
+
+cp *.py *.ogg *.png *.mp3 staging_dir/tests
+cp pywrap/*.py staging_dir/src/pywrap
+cp LICENSE.txt README.md staging_dir
+cp pip-build/* staging_dir
+
+if [[ -f build.zip ]]; then
+    rm build.zip
+fi
+pushd staging_dir
+zip  ../build.zip -r .
+popd
+
+echo "*** files in build.zip ***"
+unzip -l ./build.zip
+
+echo "*** build completed ***"
+
