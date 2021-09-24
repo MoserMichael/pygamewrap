e3ca1e226758ac78732d28e10334605c5df5c4a7
Michael Moser <moser.michael@gmail.com>
2021-09-24 11:28:52 +0300

    add type hints.

--- pywrap/pywrap.py
+++ pywrap/pywrap.py
@@ -1,6 +1,7 @@
 from random import gammavariate
 from types import prepare_class
-from typing import NamedTuple
+from typing import NamedTuple, Tuple, Callable, Type
+
 import pygame
 
 from pygame.locals import (
@@ -20,21 +21,21 @@ class PygameGlobal:
     game = None
 
     @staticmethod
-    def get_game():
+    def get_game() -> 'PyGame':
         return PygameGlobal.game
 
     @staticmethod
-    def set_game(game):
+    def set_game(game : 'PyGame') -> None:
         PygameGlobal.game = game
 
 
 class CacheableSprite(pygame.sprite.Sprite):
 
-    def __init__(self, layer):
+    def __init__(self, layer : int):
         self._layer  = layer
         super(CacheableSprite, self).__init__()
  
-    def kill(self):
+    def kill(self) -> None:
         # remove sprite from all groups
         super().kill()
 
@@ -45,7 +46,7 @@ class CacheableSprite(pygame.sprite.Sprite):
 
 class AnimatedSprite(CacheableSprite):
 
-    def __init__(self, imageFileList, transparentBackgroundColor, initPos, layer=0):
+    def __init__(self, imageFileList : list, transparentBackgroundColor : Tuple[int, int, int], initPos: Tuple[int, int], layer : int = 0):
         super(AnimatedSprite, self).__init__(layer)
 
         self.images = list()
@@ -60,7 +61,7 @@ class AnimatedSprite(CacheableSprite):
         self.current_frame = -1
         
     # draws the buffer on the current screen buffer.
-    def draw_on_buffer(self, game):
+    def draw_on_buffer(self, game : 'PyGame') -> None:
         self.current_frame = (self.current_frame + 1) % len(self.images)
         game.screen.blit(self.images[ self.current_frame ], self.rect)      
 
@@ -70,7 +71,7 @@ class ImageSprite(CacheableSprite):
 
     # sprite constructor, loads an image file from imageFile, with the transparent color transparentBackgroundColor
     # placed to the initial position initPos
-    def __init__(self, imageFile, transparentBackgroundColor, initPos, layer=0):
+    def __init__(self, imageFile : str, transparentBackgroundColor : Tuple[int,int,int], initPos : Tuple[int,int], layer : int =0):
         super(ImageSprite, self).__init__(layer)
 
         #self.surf = pygame.image.load(imageFile).convert()
@@ -79,7 +80,7 @@ class ImageSprite(CacheableSprite):
         self.rect = self.surf.get_rect(center = initPos)
 
     # draws the buffer on the current screen buffer.
-    def draw_on_buffer(self, game):
+    def draw_on_buffer(self, game : 'PyGame') -> None:
         game.screen.blit(self.surf, self.rect)      
 
    
@@ -151,31 +152,31 @@ class PyGame:
 
         PygameGlobal.set_game(self)
 
-    def screen_width(self):
+    def screen_width(self) -> int:
         return self.display_width
 
-    def screen_height(self):
+    def screen_height(self) -> int:
         return self.display_height
 
 
     # add event handler
-    def add_event_handler(self, eventType, handlerFunc):
+    def add_event_handler(self, eventType, handlerFunc : Callable[['PyGame'], None]) -> None:
         self.event_handler[ eventType ] = handlerFunc
 
     # add key down event handler
-    def add_key_down_event(self, key, handlerFunc):
+    def add_key_down_event(self, key : int, handlerFunc : Callable[['PyGame'], None]) -> None:
         self.key_down_handlers[ key ] = handlerFunc
 
     # add key up event handler
-    def add_key_up_event(self, key, handlerFunc):
+    def add_key_up_event(self, key : int, handlerFunc: Callable[['PyGame'], None]) -> None:
         self.key_up_handlers[ key ] = handlerFunc
 
     # add key pressed handler, is being called on each frame if the key is still pressed.
-    def add_key_pressed_event(self, key, handlerFunc):
+    def add_key_pressed_event(self, key : int, handlerFunc: Callable[['PyGame'], None]) -> None:
         self.key_pressed_handlers[ key ] = handlerFunc
 
     # add a timer event, the handlerFunc function argument will be called once per timeInMillisecond milliseconds.
-    def add_timer_event(self, timeInMillisecond, handlerFunc):
+    def add_timer_event(self, timeInMillisecond : int, handlerFunc: Callable[['PyGame'], None]) -> None:
         timerVal = self.last_timer_event
         self.add_event_handler( timerVal, handlerFunc)
         pygame.time.set_timer(timerVal, timeInMillisecond)
@@ -184,11 +185,11 @@ class PyGame:
 
 
     # add a sprite the the group of all sprites, Note that the sprite is being rendered according to its layer (see ImageSprite init)
-    def add_sprite(self, player):
+    def add_sprite(self, player : pygame.sprite.Sprite) -> None:
         self.all_sprites.add(player)
 
     # run the game loop
-    def run(self):
+    def run(self) -> None:
         while self.running:
             
             # start of frame, draw the empty stage  here.
@@ -242,37 +243,37 @@ class PyGame:
         #pygame.quit()
 
     # calling this function will cause the game loop to exit, provided that run has been called earlier.
-    def exit(self):
+    def exit(self) -> None:
         self.running = False    
 
     # called on the beginning of each frame; here you can override to fill in the backgraound picture.
-    def on_start_frame(self):
+    def on_start_frame(self) -> None:
         self.screen.fill((135, 206, 250))
 
     # finished the frame and flipped the display, now it's time to test for collisions.
-    def on_colission_test(self):
+    def on_colission_test(self) -> None:
         pass
 
     # set the current font that is used.
-    def set_font(self, font_name, font_size):
+    def set_font(self, font_name : str, font_size : int) -> None:
         self.font_name = font_name
         self.font_size = font_size
         self.font = pygame.font.SysFont(font_name, font_size)
 
     # set color of text
-    def set_text_colors(self, fgcolor, bgcolor = None):
+    def set_text_colors(self, fgcolor : Tuple[int,int,int], bgcolor : Tuple[int,int,int] = None) -> None:
         self.fgcolor = fgcolor
         self.bgcolor = bgcolor
 
     # print a line of text on the screen
-    def print(self, x, y,  *args):
+    def print(self, x : int, y : int,  *args) -> None:
         msg = ' '.join(map(str, args))
         text_surface = self.font.render(msg, True, self.fgcolor, self.bgcolor)
         self.screen.blit(text_surface, (x, y))
 
 
     # print a lot of text on the screen, text that does not fit the line is shown on the next line.
-    def print_text(self, x,y, *args):
+    def print_text(self, x : int, y : int, *args) -> None:
 
         msg = ' '.join(map(str, args))
 
@@ -292,7 +293,7 @@ class PyGame:
 
          
     # print text on the screen, wait for the user to spress space
-    def print_dialog(self, *args):
+    def print_dialog(self, *args) -> None:
         
         self.screen.fill((135, 206, 250))
         self.print_text(0,0, *args)
@@ -307,7 +308,7 @@ class PyGame:
                     break
             self.clock.tick(30)
 
-    def load_image(self, fileName):
+    def load_image(self, fileName : str) -> None:
         if not fileName in self.map_file_to_image:
             image = pygame.image.load(fileName)
             if image == None:
@@ -317,7 +318,7 @@ class PyGame:
         
         return self.map_file_to_image[ fileName ] 
 
-    def play_sound(self, fileName, loops = 0):
+    def play_sound(self, fileName : str, loops : int = 0) -> None:
         if not fileName in self.map_file_to_sound:
             sound = pygame.mixer.Sound(fileName)
             self.map_file_to_sound[ fileName ] = sound
@@ -329,14 +330,14 @@ class PyGame:
         else:
             print("sound: ", fileName, "not found!")
 
-    def play_background_sound(self, fileName):
+    def play_background_sound(self, fileName : str) -> None:
         self.play_sound(fileName, -1)
 
     def stop_all_sounds(self):
         for k,v in self.map_file_to_sound.items():
             v.stop()
 
-    def get_cached_sprite(self, sprite_class):
+    def get_cached_sprite(self, sprite_class : Type) -> None:
         type_name = str(sprite_class)
         if  type_name in self.map_type_to_spritecache:
             cache = self.map_type_to_spritecache[ type_name ]
@@ -347,7 +348,7 @@ class PyGame:
 
 
 
-    def add_to_cache_(self, sprite):
+    def add_to_cache_(self, sprite : CacheableSprite) -> None:
 
         # check if sprite is cacheable - it is if it has the reuse method
         reuse_method = getattr(sprite,"reuse", None)
