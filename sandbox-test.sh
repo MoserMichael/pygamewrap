848d173b73d194b2de3b5a560f39e4ae55a9feb9
Michael Moser <moser.michael@gmail.com>
2021-10-21 06:18:17 +0300

    fix pip install. uff.

---
+++ sandbox-test.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -e 
+
+rm -rf tst || true
+mkdir tst
+pushd tst
+# test installation of module in virtual environment
+virtualenv my-venv
+source my-venv/bin/activate
+
+cp ../*.py ../*.png ../*.ogg ../*.mp3 .       
+pip3 install pygamewrap
+
+python3 12-add-animated-sprite.py
+
+echo "everything is fine. test passed"
+
+deactivate
+
+#rm -rf my-venv
+
+popd tst
